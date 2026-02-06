2c6396b38a154aee3a01b86189c3cfa880b0bfe7
elieelk29 <ebambaelierufus@gmail.com>
2026-02-06 12:41:58 +0100

    Add initial pong game assembly code

---
+++ pong.asm
@@ -0,0 +1,77 @@
+
+
+        ORG 2000h           
+P1Y:    DB 12      ; Position Y raquette gauche (0-23)
+P2Y:    DB 12      ; Position Y raquette droite
+BALLX:  DB 16      ; Position X balle (0-31)
+BALLY:  DB 12      ; Position Y balle (0-23)
+VX:     DB 1       ; Vitesse X : -1=gauche, +1=droite
+VY:     DB 1       ; Vitesse Y : -1=haut, 0, +1=bas
+SCORE1: DB 0       ; Score joueur 1
+SCORE2: DB 0       ; Score joueur 2
+
+
+; SECTION DU CODE
+
+
+        ORG 2000h           ; On retourne à l'adresse du code
+
+; -------------------------------------------------
+; POINT D'ENTRÉE DU PROGRAMME
+; -------------------------------------------------
+START:
+        ; Initialisation des variables
+        CALL INIT_VARIABLES
+
+        ; Boucle principale du jeu
+MAIN_LOOP:
+       
+
+        ; Petite temporisation (vide pour l'instant)
+        CALL DELAY
+
+        ; Recommencer la boucle
+        JP MAIN_LOOP
+
+
+; SOUS-ROUTINES
+
+
+; Initialise toutes les variables
+INIT_VARIABLES:
+        ; Initialisation raquettes
+        LD A, 12
+        LD (P1Y), A
+        LD (P2Y), A
+
+        ; Initialisation balle au centre
+        LD A, 16
+        LD (BALLX), A
+        LD A, 12
+        LD (BALLY), A
+
+        ; Vitesse initiale
+        LD A, 1
+        LD (VX), A
+        LD (VY), A
+
+        ; Scores à zéro
+        LD A, 0
+        LD (SCORE1), A
+        LD (SCORE2), A
+
+        RET
+
+; Temporisation simple (vide pour l'instant)
+DELAY:
+       
+-
+
+; Caractères pour l'affichage (à définir plus tard)
+CHAR_RAQUETTE:  DB 'I'
+CHAR_BALLE:     DB 'O'
+CHAR_VIDE:      DB ' '
+CHAR_MUR:       DB '-'
+
+
+        END START           ; Point d'entrée = START
